3931426b85765c5c912b2670b3ea796a5f708265
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2023-10-21 17:11:03 -0300

    feat: create fetch to fakeAPI to render articles

--- src/modules/homepage/components/ArticlesSection.vue
+++ src/modules/homepage/components/ArticlesSection.vue
@@ -1,12 +1,44 @@
 <template>
   <div>
     <h1>Lastest Article</h1>
+    <div v-for="article in articles.slice().reverse()" :key="article.id">
+      <img :src="getImgUrl(article.image)" alt="article.title" />
+      <p>{{ article.author }}</p>
+      <h2>{{ article.title }}</h2>
+      <p>{{ article.content }}</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+  import { type IArticle } from '@/core/models/db/IArticle';
+
   export default {
     name: 'ArticlesSection',
+    setup() {
+      const getImgUrl = (pic: string): string => {
+        return new URL(`../../../assets/images/${pic}`, import.meta.url).href
+      }
+
+      return { getImgUrl }
+    },
+    data() {
+      return{
+        articles: [] as IArticle[]
+      }
+    },
+    methods: {
+      fetchArticles() {
+        fetch('http://localhost:3001/articles')
+          .then(response => response.json())
+          .then(data => {
+            this.articles = data.slice(-4)
+          })
+      },
+    },
+    mounted() {
+        this.fetchArticles()
+    },
   }
 </script>
 
