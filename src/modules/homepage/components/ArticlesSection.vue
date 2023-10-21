b15d4f5b28d38d3e711d0a43f6fbbf161b573ad8
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2023-10-21 15:26:21 -0300

    feat: create base ArticleSection

---
+++ src/modules/homepage/components/ArticlesSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Lastest Article</h1>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'ArticlesSection',
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/modules/homepage/views/HomePage.vue
+++ src/modules/homepage/views/HomePage.vue
@@ -1,17 +1,20 @@
 <template>
   <HeroSection />
   <FeaturesSection />
+  <ArticlesSection />
 </template>
 
 <script lang="ts">
 import HeroSection from '@/modules/homepage/components/HeroSection.vue';
 import FeaturesSection from '@/modules/homepage/components/FeaturesSection.vue';
+import ArticlesSection from '../components/ArticlesSection.vue';
 
 export default {
   name: 'HomePage',
   components: {
     HeroSection,
-    FeaturesSection
+    FeaturesSection,
+    ArticlesSection
   }
 }
 
