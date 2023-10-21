03d617c804bb7fb85ddfdce85c85273a0a5b0fe2
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2023-10-21 10:20:34 -0300

    feat: create common RequestInviteButton for whole app

---
+++ src/core/components/RequestInviteButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button>Request invite</button>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'RequestInviteButton'
+  }
+
+</script>
+
+<style scoped>
+  button {
+    background-image: linear-gradient(90deg, var(--vt-c-lime-green), var(--vt-c-light-blue));
+    color: var(--vt-c-white);
+    border: none;
+    border-radius: 30px;
+    padding: 15px 40px;
+    font-size: 1em;
+    font-weight: 700;
+    cursor: pointer;
+  }
+
+  button:hover {
+    opacity: 0.6;
+  }
+</style>
