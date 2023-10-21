78a5b83b0c2a58ceea76fca4cad63bcbc3348f8c
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2023-10-21 12:29:58 -0300

    feat: create all html for FeatureSection component

---
+++ src/modules/homepage/components/FeaturesSection.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>Why choose Easybank?</h1>
+    <p>
+      We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before.
+    </p>
+
+    <div class="features__area">
+      <div>
+        <img src="@/assets/icons/icon-online.svg" alt="Online Banking" />
+        <h2>Online Banking</h2>
+        <p>
+          Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.
+        </p>
+      </div>
+
+      <div>
+        <img src="@/assets/icons/icon-budgeting.svg" alt="Simple Budgeting" />
+        <h2>Simple Budgeting</h2>
+        <p>
+          See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.
+        </p>
+      </div>
+      
+      <div>
+        <img src="@/assets/icons/icon-onboarding.svg" alt="Fast Onboarding" />
+        <h2>Fast Onboarding</h2>
+        <p>
+          We don’t do branches. Open your account in minutes online and start taking control of your finances right away.
+        </p>
+      </div>
+
+      <div>
+        <img src="@/assets/icons/icon-api.svg" alt="Open API" />
+        <h2>Open API</h2>
+        <p>
+          Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'FeaturesSection',
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/modules/homepage/views/HomePage.vue
+++ src/modules/homepage/views/HomePage.vue
@@ -1,14 +1,17 @@
 <template>
   <HeroSection />
+  <FeaturesSection />
 </template>
 
 <script lang="ts">
 import HeroSection from '@/modules/homepage/components/HeroSection.vue';
+import FeaturesSection from '@/modules/homepage/components/FeaturesSection.vue';
 
 export default {
   name: 'HomePage',
   components: {
-    HeroSection
+    HeroSection,
+    FeaturesSection
   }
 }
 
