073f520650a4f529154a84b47ce0c9d032afdc70
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2023-10-31 12:37:11 -0300

    test: ensures that fetches articles are rendered

---
+++ src/modules/homepage/components/__tests__/ArticlesSection.spec.ts
@@ -0,0 +1,54 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it, vi } from 'vitest';
+import ArticlesSection from '../ArticlesSection.vue'
+
+describe('ArticlesSection', () => {
+  it('ensure fetches articles are rendered', async () => {
+    const articles = [
+      {
+        id: 1,
+        title: 'Article 1',
+        author: 'Author 1',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        image: 'article1.jpg'
+      },
+      {
+        id: 2,
+        title: 'Article 2',
+        author: 'Author 2',
+        content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem.',
+        image: 'article2.jpg'
+      },
+      {
+        id: 3,
+        title: 'Article 3',
+        author: 'Author 3',
+        content: 'At vero eos et accusamus et iusto odio dignissimos ducimus.',
+        image: 'article3.jpg'
+      },
+      {
+        id: 4,
+        title: 'Article 4',
+        author: 'Author 4',
+        content: 'Eaque ipsa quae ab illo inventore veritatis et quasi architecto.',
+        image: 'article4.jpg'
+      }
+    ]
+    
+    vi.spyOn(window, 'fetch').mockImplementationOnce(() => {
+      return Promise.resolve({
+        json: () => Promise.resolve(articles)
+      } as Response)
+    })
+
+    const wrapper = mount(ArticlesSection)
+
+    setTimeout(() => {
+      expect(wrapper.findAll('.article')).toHaveLength(4)
+      expect(wrapper.find('.article__title').text()).toBe('Article 4')
+      expect(wrapper.find('.article__author').text()).toBe('By Author 4')
+      expect(wrapper.find('.article__text').text()).toBe('Eaque ipsa quae ab illo inventore veritatis et...')
+      expect(wrapper.find('.article__image').attributes('src')).toBe('http://localhost/assets/images/article4.jpg')
+    }, 1000)
+  })
+})
