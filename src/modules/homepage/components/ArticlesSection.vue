2b8d5bd6437e98ec44cee465618e073cc14cb2b0
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2023-11-06 14:58:54 -0300

    style: create desktop style for articles section

--- src/modules/homepage/components/ArticlesSection.vue
+++ src/modules/homepage/components/ArticlesSection.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="article__section">
-    <h1 class="article__section__title">Lastest Article</h1>
-    <div class="articles__area">
-      <div v-for="article in articles.slice().reverse()" :key="article.id" class="article">
-        <img :src="getImgUrl(article.image)" alt="article.title" class="article__image" />
-        <div class="article__content">
-          <p class="article__author">{{ "By " + article.author }}</p>
-          <h2 class="article__title">{{ article.title }}</h2>
-          <p class="article__text">{{ summaryText(article.content, 130) }}</p>
+    <div class="article__area__content">
+      <h1 class="article__section__title">Lastest Article</h1>
+      <div class="articles__area">
+        <div v-for="article in articles.slice().reverse()" :key="article.id" class="article">
+          <img :src="getImgUrl(article.image)" alt="article.title" class="article__image" />
+          <div class="article__content">
+            <p class="article__author">{{ "By " + article.author }}</p>
+            <h2 class="article__title">{{ article.title }}</h2>
+            <p class="article__text">{{ summaryText(article.content, 125) }}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -109,10 +111,50 @@
     color: var(--color-heading);
   }
 
+  .article:hover {
+    cursor: pointer;
+  }
+
+  .article__title:hover {
+    background-image: linear-gradient(90deg, var(--vt-c-lime-green), var(--vt-c-light-blue));
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+    cursor: pointer;
+  }
+
   .article__text {
     font-size: 0.8em;
     line-height: 1.4em;
     color: var(--color-text);
+  }
+
+  @media (min-width: 1024px) {
+    .article__area__content {
+      max-width: 1200px;
+      padding: 0 25px;
+      margin: 0 auto;
+      justify-content: space-around;
+    }
+
+    .article__section__title {
+      text-align: left;
+      margin-left: 0;
+      font-size: 2.2em;
+      margin-top: 60px;
+      margin-bottom: 40px;
+    }
+
+    .articles__area {
+      display: flex;
+      margin: 0;
+      padding: 0;
+      gap: 20px;
+    }
+
+    .article {
+      width: 270px;
+    }
   }
 
 </style>
